0b8ef0eba23153dac68c582bb07001ff81500cff
maxsoulfly <max.kibinimat@gmail.com>
2025-02-12 20:14:10 +0200

    palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = (word) => {
+    // much more readable than /[^a-z0-9]/ - cannot understand for now
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789'; 
 
+    const wordToCheck = word
+        .toLowerCase()
+        .split('')
+        .filter(letter => alphanumerical.includes(letter))
+        .join('');
+
+    const reversedWord = wordToCheck
+        .split('')
+        .reverse()
+        .join('');
+
+    return wordToCheck===reversedWord;
 };
 
 // Do not edit below this line
